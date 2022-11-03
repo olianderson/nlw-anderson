74d5b92dbe843bf00fb8315cc9b66c7f4bb94162
olianderson <olianderson1@gmail.com>
2022-11-03 20:22:12 -0300

    nlw dia 4

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.4
 function createCard(date, day, games) {
+  delay = delay + 0.4
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -19,17 +21,20 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img align="center" src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-  <main id="cards">
-    ${createCard(
-      "21/11",
-      "quinta",
-      createGame("England", "10:00", "Iran") +
-        createGame("United States", "16:00", "Wales") +
-        createGame("Senegal", "13:00", "Netherlands")
-    )}
-  </main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "DOM", createGame("Qatar", "13:00", "Ecuador")) +
+  createCard(
+    "21/11",
+    "SEG",
+    createGame("England", "10:00", "Iran") +
+      createGame("United States", "16:00", "Wales") +
+      createGame("Senegal", "13:00", "Netherlands")
+  ) +
+  createCard(
+    "22/11",
+    "TER",
+    createGame("Argentina", "07:00", "Saudi Arabia") +
+      createGame("Mexico", "13:00", "Poland") +
+      createGame("Denmark", "10:00", "Tunisia") +
+      createGame("France", "16:00", "Australia")
+  )
